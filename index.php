9930029b8aa39421da79bf9f66f47e41801e585f
Anton Eliasson <devel@antoneliasson.se>
2016-01-28 10:39:06 +0100

    Set content length

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $cat = file_get_contents('http://thecatapi.com/api/images/get?format=src&type=png&size=med');
 
 header('Content-Type: image/png');
-
+header('Content-Length: ' + strlen($cat));
 echo $cat;
 ?>
