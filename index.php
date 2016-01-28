dac7b4a26541560f00dddf678ccd75bd0283274a
Anton Eliasson <devel@antoneliasson.se>
2016-01-28 10:56:44 +0100

    Add cat caching

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
-$cat = file_get_contents('http://thecatapi.com/api/images/get?format=src&type=png&size=med');
+define("cache_filename", "/tmp/kittenproxy.png");
+define("freshness_seconds", 5*60);
 
-header('Content-Type: image/png');
-header('Content-Length: ' . strlen($cat));
-echo $cat;
+function fresh_cat() {
+    $stat = stat(cache_filename);
+    return time() - $stat['mtime'] < freshness_seconds;
+}
+
+function get_new_cat() {
+    $cat = file_get_contents('http://thecatapi.com/api/images/get?format=src&type=png&size=med');
+    file_put_contents(cache_filename, $cat);
+}
+
+function serve_cat($cat) {
+    header('Content-Type: image/png');
+    header('Content-Length: ' . strlen($cat));
+    echo $cat;
+}
+
+if (fresh_cat()) {
+    $cat = read_cat();
+} else {
+    $cat = get_new_cat();
+}
+serve_cat($cat);
 ?>
